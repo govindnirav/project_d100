9935187904fd7ca6d697ac22e2d99b330539e513
govindnirav <87795211+govindnirav@users.noreply.github.com>
2024-12-11 16:47:27 +0000

    Feat: function to save cleaned data as parquet

--- project_d100/data/__init__.py
+++ project_d100/data/__init__.py
@@ -1,4 +1,13 @@
 from ._clean import denormalise, extract_day, move_col, ren_year
 from ._load import load_data, summary
+from ._save import save_parquet
 
-__all__ = ["load_data", "summary", "extract_day", "denormalise", "ren_year", "move_col"]
+__all__ = [
+    "load_data",
+    "summary",
+    "extract_day",
+    "denormalise",
+    "ren_year",
+    "move_col",
+    "save_parquet",
+]
